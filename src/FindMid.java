30e8f0df1a93a3d85896f8005c2d99fa727f6483
Vishwas <vishwas@kirana11.com>
2018-08-27 19:40:12 +0530

    Implemented Finding middle element of linked list

---
+++ src/FindMid.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/description/
+ *
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ */
+
+public class FindMid {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
